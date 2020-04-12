1d51b6d99edf4f6bc0a0a0c74779ed0ab3583164
kyle67 <daniels.kyle@gmail.com>
2020-04-12 02:19:10 -0400

    Modified head section with bootstrap

--- react_node-aws/components/layout.js
+++ react_node-aws/components/layout.js
@@ -1,4 +1,17 @@
+import Head from 'next/head'
+
 const Layout = ({children})=>{
+
+    const head =()=>(
+        <link 
+        
+        rel="stylesheet" 
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+        crossorigin="anonymous"
+        />
+    );
+
     const nav = ()=>(
 <ul className="nav">
     <li className="nav-item">
@@ -15,9 +28,13 @@ const Layout = ({children})=>{
 </ul>
     );
       
-return <React.Fragment>{nav()} {children}
-</React.Fragment>;
+return (
 
+<React.Fragment>
+  {head()} {nav()} {children}
+</React.Fragment>
+
+    );  
 };
 
 export default Layout;
