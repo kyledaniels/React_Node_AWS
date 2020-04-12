26ab84f5a7ac0c8bb8a0dead496c226c6aacf049
kyle67 <daniels.kyle@gmail.com>
2020-04-12 11:15:10 -0400

    set up login and register pages

---
+++ react_node-aws/pages/login.js
@@ -0,0 +1,5 @@
+import Layout from '../components/layout'
+
+const Login = () => <Layout>login page</Layout>;
+
+export default Login;
---
+++ react_node-aws/pages/register.js
@@ -0,0 +1,5 @@
+import Layout from '../components/layout'
+
+const Register = () => <Layout>register page</Layout>
+
+export default Register;
