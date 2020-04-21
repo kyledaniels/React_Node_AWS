064290a4476a7a67860a48bd6b8263073f9d0f7e
kyle67 <daniels.kyle@gmail.com>
2020-04-21 18:38:45 -0400

    created registration form

--- react_node-aws/components/layout.js
+++ react_node-aws/components/layout.js
@@ -14,12 +14,17 @@ const Layout = ({children})=>{
 
     const head =()=>(
 
-        <React.Fragment><link 
+       <React.Fragment>
+            
+    <link 
         rel="stylesheet" 
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
         crossorigin="anonymous"
         />
+
+    <link rel="stylesheet" href="/static/css/styles.css"/>
+
         </React.Fragment>
         
     );
--- react_node-aws/pages/register.js
+++ react_node-aws/pages/register.js
@@ -1,6 +1,54 @@
+import {useState} from 'react'
 import Layout from '../components/layout'
 
 
-const Register = () => <Layout>register page</Layout>
+const Register = () => {
+
+    const [state, setState] = useState({
+        name:'',
+        email:'',
+        password:'',
+        error:'',
+        success:'',
+        buttonText:'Register'
+
+    })
+
+       const {name,email, password,error, success, buttonText}=state;
+       
+       const handleChange = (name)=> (e)=>{
+             setState({...state,[name]:e.target.value,error:'',success:'',buttonText:'Register'})
+       }
+
+       const handleSubmit = ()=>{
+
+       }
+
+       const registerform=()=><form onSubmit={handleSubmit}>
+           <div className= "form-group">
+               <input onChange={handleChange('name')} type="text" className="form-control" placeholder="Type your name"/>
+           </div>
+           <div className= "form-group">
+               <input onChange={handleChange('email')}type="email" className="form-control" placeholder="Type your email"/>
+           </div>
+           <div className= "form-group">
+               <input onChange={handleChange('password')}type="password" className="form-control" placeholder="Type your password"/>
+           </div>
+           <div className= "form-group">
+              <button className= "btn btn-outline-warning">{buttonText}</button>
+           </div>
+       </form>;
+
+       return(
+           <layout>
+               <div className="col-md-6 offset-md-3">
+                   <h1>Register</h1>
+                   <br/>
+                   {registerform()}
+               </div>
+           </layout>
+       )
+
+}
 
 export default Register;
