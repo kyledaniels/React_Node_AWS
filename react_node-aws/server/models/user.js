30b61853925c7bfe9ac03b2a10de252086f0d7d4
kyle67 <daniels.kyle@gmail.com>
2020-07-02 19:02:10 -0400

    created function to generate salt

--- react_node-aws/server/models/user.js
+++ react_node-aws/server/models/user.js
@@ -61,10 +61,16 @@ const userSchema = new mongoose.Schema({
           encryptPassword:function(password){
               if(password) return ''
               try {
-                  return crypto
+                  return crypto.createHmac('sha1', this.salt)
+                              .update(password)
+                              .digest('hex')
               } catch (err){
                   return ''
               }
+          },
+
+          makeSalt: function(){
+              return Math.round(new Date().valueOf() * Math.random())+ '';
           }
    };
 
